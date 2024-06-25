42389f008c5a4746f39579016e2425b782c3c4dc
abhay kumar <abhaykumar2871999@gmail.com>
2024-06-25 22:08:09 +0530

    in header nav item added

--- src/App.jsx
+++ src/App.jsx
@@ -17,12 +17,16 @@ function App() {
     </div>
     <div className="header_navigation">
         <div className="nav_item">
-          
+          <span className="nav_itemLineOne">Hello Guest</span>
+          <span className="nav_itemLineTwo">Sign in</span>
         </div>
         <div className="nav_item">
-
+          <span className="nav_itemLineOne">Your</span>
+          <span className="nav_itemLineTwo">Shop</span>
         </div>
         <div className="nav_item">
+          <span className="nav_itemLineOne">Basket</span>
+          <span className="nav_itemLineTwo">0</span>
 
         </div>
       

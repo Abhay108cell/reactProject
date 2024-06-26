222658ed94f3c6213b2a155e628e2ad38f297c95
abhay kumar <abhaykumar2871999@gmail.com>
2024-06-26 22:28:26 +0530

    product added in  Home.jsx

--- src/Home.jsx
+++ src/Home.jsx
@@ -8,6 +8,22 @@ function Home(){
         <div className="home">
           <div className="home__container">
             <img src="https://www.vocso.com/blog/wp-content/uploads/2022/02/eCommerce-Website-Features-1920-x-1080-1024x576.jpg" alt="" className="home__image" />
+
+          <div className="home__row">
+           < Product/>
+           < Product/>
+          </div>
+          <div className="home__row">
+           < Product/>
+           < Product/>
+           < Product/>
+          </div>
+          <div className="home__row">
+           < Product/>
+          </div>
+
+
+
           </div>
         </div>
     </div>

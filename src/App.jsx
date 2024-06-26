76baa13774822f610af8623736bae38b4123f501
abhay kumar <abhaykumar2871999@gmail.com>
2024-06-26 22:22:16 +0530

    init Home.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import  Header from'./Header.jsx'
+import Home from './Home.jsx'
 
 function App() {
   const [count, setCount] = useState(0)

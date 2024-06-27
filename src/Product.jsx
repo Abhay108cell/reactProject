f04fa0e7ab452600da7044eafa93ba586eae0339
abhay kumar <abhaykumar2871999@gmail.com>
2024-06-27 22:25:47 +0530

    product Title,info,rating,price created in product.jsx

--- src/Product.jsx
+++ src/Product.jsx
@@ -3,7 +3,14 @@ import "./Product.css"
 function Product (){
   return(
     <div>
-      <div className="product"></div>
+      <div className="product">
+        <div className="product__info">
+          <p>Title</p>
+          <p className="product__price">$300</p>
+          <p className="product__rating">*****</p>
+        </div>
+        <img src="https://people.com/thmb/I30mqUIY6bvjgQejCcrOvQG8LSY=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():focal(999x0:1001x2):format(webp)/movers-shakers-kitchen-roundup-tout-2000-68041dc948184815b78cab75a4fbd088.jpg" alt="" />
+      </div>
 
     </div>
   )

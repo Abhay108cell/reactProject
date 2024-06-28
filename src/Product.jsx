e4b8a29d76d18b677e5b09ba72c1a044d45f98ba
abhay kumar <abhaykumar2871999@gmail.com>
2024-06-28 18:15:42 +0530

    title,price,image props in Product.jsx

--- src/Product.jsx
+++ src/Product.jsx
@@ -1,18 +1,18 @@
 import React from "react";
 import "./Product.css"
-function Product (){
+function Product ({id, title, price, rating, image}){
   return(
     <div>
       <div className="product">
         <div className="product__info">
-          <p>"Bennett Mystic 15.6 inch Laptop Shoulder Messenger Sling Office Bag, Water Repellent Fabric for Men and Women (Blue)"</p>
+          <p>{title}</p>
           <p className="product__price">
             <small>$</small>
-            <strong>99</strong>
+            <strong>{price}</strong>
           </p>
           <p className="product__rating">*****</p>
         </div>
-        <img src="https://images-na.ssl-images-amazon.com/images/I/71mEsHyzSCL._SL1000_.jpg" alt="" />
+        <img src={image} alt="" />
 
        <button>Add to Basket</button>
 

d1ee6314995de908898c57adfb71874db212bc14
abhay kumar <abhaykumar2871999@gmail.com>
2024-06-28 22:29:37 +0530

    finding bug in program

--- src/App.jsx
+++ src/App.jsx
@@ -1,69 +1,23 @@
-// import { useState } from 'react'
-// // import reactLogo from './assets/react.svg'
-// // import viteLogo from '/vite.svg'
-// import './App.css'
-// import  Header from'./Header.jsx'
-// import Home from './Home.jsx'
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import  Header from'./Header.jsx'
+import Home from './Home.jsx'
 // import Checkout from './Checkout.jsx'
 // import {BrowserRouter as Router, Route, Switch} from "react-router-dom"
-
-// function App() {
-//   const [count, setCount] = useState(0)
-
-//   return (
-//     <>
-//   <div className="App">
-//     <Router>
-//       <Switch>
-//         <Route path="/checkout">
-//         <Header/>
-//         <Checkout/>
-//         </Route>
-//         <Route path="/">
-//         <Header/>
-//         <Home/>
-//         </Route>
-//       </Switch>
-//     </Router>
-//       {/* <Header/>
-//       <Home/> */}
-//    </div>
-//     </>
-//   )
-// }
-
-// export default App
-
-
-import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
-import './App.css'
-import Header from './Header.jsx'
-import Home from './Home.jsx'
-import Checkout from './Checkout.jsx'
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    
-      <div className="App">
-        <Router>
-          <Switch>
-            <Route path="/checkout">
-              <Header />
-              <Checkout />
-            </Route>
-            <Route path="/">
-              <Header />
-              <Home />
-            </Route>
-          </Switch>
-        </Router>
-      </div> 
-
+    <>
+  <div className="App">
+      <Header/>
+      {/* <Home/> */}
+   </div>
+    </>
   )
 }
+
 export default App

5bc4cd496f89e3c517ce1b39d63d718d711ab2cc
abhay kumar <abhaykumar2871999@gmail.com>
2024-06-28 22:24:15 +0530

    using router in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,69 @@
+// import { useState } from 'react'
+// // import reactLogo from './assets/react.svg'
+// // import viteLogo from '/vite.svg'
+// import './App.css'
+// import  Header from'./Header.jsx'
+// import Home from './Home.jsx'
+// import Checkout from './Checkout.jsx'
+// import {BrowserRouter as Router, Route, Switch} from "react-router-dom"
+
+// function App() {
+//   const [count, setCount] = useState(0)
+
+//   return (
+//     <>
+//   <div className="App">
+//     <Router>
+//       <Switch>
+//         <Route path="/checkout">
+//         <Header/>
+//         <Checkout/>
+//         </Route>
+//         <Route path="/">
+//         <Header/>
+//         <Home/>
+//         </Route>
+//       </Switch>
+//     </Router>
+//       {/* <Header/>
+//       <Home/> */}
+//    </div>
+//     </>
+//   )
+// }
+
+// export default App
+
+
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
 import './App.css'
-import  Header from'./Header.jsx'
+import Header from './Header.jsx'
 import Home from './Home.jsx'
+import Checkout from './Checkout.jsx'
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-  <div className="App">
-      <Header/>
-      <Home/>
-   </div>
-    </>
+    
+      <div className="App">
+        <Router>
+          <Switch>
+            <Route path="/checkout">
+              <Header />
+              <Checkout />
+            </Route>
+            <Route path="/">
+              <Header />
+              <Home />
+            </Route>
+          </Switch>
+        </Router>
+      </div> 
+
   )
 }
-
 export default App
--- src/Checkout.jsx
+++ src/Checkout.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import "./Checkout.css"
 
-export default function Checkout() {
+ function Checkout() {
   return (
     <div>
       <h1>hello world</h1>
     </div>
   )
 }
+export default Checkout;

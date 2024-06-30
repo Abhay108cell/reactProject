f10c1a86f0420a5e5b26ad6a3dcd9799e8fcecfb
abhay kumar <abhaykumar2871999@gmail.com>
2024-06-30 23:13:06 +0530

    added image in left div checkout.jsx

--- src/Checkout.jsx
+++ src/Checkout.jsx
@@ -7,10 +7,11 @@ function Checkout() {
     <div>
       <div className="checkout">
         <div className="checkout__left">
+        <img src="https://images-na.ssl-images-amazon.com/images/G/02/UK_CCMP/TM/OCC_Amazon1._CB423492668_.jpg" alt="" className="checkout__ad" />
 
         </div>
         <div className="checkout__right">
-          
+
         </div>
       </div>
     </div>

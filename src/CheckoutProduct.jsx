8fb82d72cf0e8c4b2fead01a40d5aaee2a005b69
abhay kumar <abhaykumar2871999@gmail.com>
2024-06-30 23:31:35 +0530

    finding bug in checkoutProduct.jsx

--- src/CheckoutProduct.jsx
+++ src/CheckoutProduct.jsx
@@ -5,7 +5,7 @@ function CheckoutProduct() {
   return (
     <div>
       <div className="checkoutProduct">
-        <img src="" alt="" />
+        <img src="https://images-na.ssl-images-amazon.com/images/G/02/UK_CCMP/TM/OCC_Amazon1._CB423492668_.jpg" alt="" />
       </div>
     </div>
   )

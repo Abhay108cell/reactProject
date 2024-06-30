b49970a7d3ce1104cd8383139d6f540ad76fdbda
abhay kumar <abhaykumar2871999@gmail.com>
2024-06-30 23:32:02 +0530

    adding some basic function checkoutProduct.jsx

--- src/CheckoutProduct.jsx
+++ src/CheckoutProduct.jsx
@@ -5,7 +5,7 @@ function CheckoutProduct() {
   return (
     <div>
       <div className="checkoutProduct">
-        <img src="https://images-na.ssl-images-amazon.com/images/G/02/UK_CCMP/TM/OCC_Amazon1._CB423492668_.jpg" alt="" />
+        <img src="" alt="" />
       </div>
     </div>
   )

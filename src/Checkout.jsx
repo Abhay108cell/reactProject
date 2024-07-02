cba46d2093fa804d27de051e916e68b64cafae4f
abhay kumar <abhaykumar2871999@gmail.com>
2024-07-02 15:05:24 +0530

    working properly checkoutProduct.jsx

--- src/Checkout.jsx
+++ src/Checkout.jsx
@@ -1,11 +1,25 @@
-import React from 'react'
+import "./Checkout.css"
+import React from "react";
+import CheckoutProduct from "./CheckoutProduct";
 
 function Checkout() {
   return (
     <div>
-      <h1>Radha Raman</h1>
+      <div className="checkout__left">
+        <img
+          src="https://images-na.ssl-images-amazon.com/images/G/02/UK_CCMP/TM/OCC_Amazon1._CB423492668_.jpg"
+          alt=""
+          className="checkout__ad"
+        />
+        <div className="checkout__title">
+          <h2>Your Shopping Basket</h2>
+        </div>
+        <CheckoutProduct/>
+      </div>
+
+      <div className="checkout__right"></div>
     </div>
-  )
+  );
 }
 
 export default Checkout;
--- src/CheckoutProduct.jsx
+++ src/CheckoutProduct.jsx
@@ -5,7 +5,18 @@ function CheckoutProduct() {
   return (
     <div>
       <div className="checkoutProduct">
-        <img src="" alt="" />
+        <img src="https://images-na.ssl-images-amazon.com/images/I/71mEsHyzSCL._SL1000_.jpg" alt="" className="checkoutProduct__image" />
+        <div className="checkoutProduct__info">
+          <p className="checkoutProduct__title">The Lean Startup</p>
+          <p className="checkoutProduct__price">
+          <small>$</small>
+          <strong>60</strong>
+          </p>
+          <div className="checkoutProduct__rating">
+            <p>⭐⭐⭐⭐⭐</p>
+          </div>
+          <button className="checkoutProduct__button">Remove from Cart</button>
+        </div>
       </div>
     </div>
   )

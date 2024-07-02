78867ae7a39b37f0f1ea8e9de3bc8834e6f0b552
abhay kumar <abhaykumar2871999@gmail.com>
2024-07-02 16:55:50 +0530

    renderText value in subTotal.jsx

--- src/SubTotal.jsx
+++ src/SubTotal.jsx
@@ -4,7 +4,18 @@ import CurrencyFormat from "react-currency-format"
 function SubTotal() {
   return (
     <div>
-      <CurrencyFormat/>
+      <CurrencyFormat
+      renderText={(value)=>{
+        <>
+        <p>
+          Subtotal (0 items) : <strong>$0</strong>
+        </p>
+        <small className="subtotal__gift">
+          <input type="checkbox" /> This order contains a gift
+        </small>
+        </>
+      }}
+      />
     </div>
   )
 }

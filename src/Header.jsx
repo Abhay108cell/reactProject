aee94b99f0202132851b5fb01dcfa4493d47dd7c
abhay kumar <abhaykumar2871999@gmail.com>
2024-07-02 14:42:10 +0530

    imported link in Header.jsx

--- src/Header.jsx
+++ src/Header.jsx
@@ -3,7 +3,7 @@ import "./Header.css"
 import ShoppingBasketIcon from '@mui/icons-material/ShoppingBasket';
 import StorefrontIcon from '@mui/icons-material/Storefront';
 import SearchIcon from '@mui/icons-material/Search';
-// import { Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 function Header(){
   return(
@@ -27,12 +27,14 @@ function Header(){
           <span className="nav__itemLineOne">Your</span>
           <span className="nav__itemLineTwo">Shop</span>
         </div>
-        {/* <Link to="/checkout" style={{textDecoration: "none"}}> </Link> */}
+        <Link to="/checkout" style={{textDecoration: "none"}}>
         <div className="nav__itemBasket">
          < ShoppingBasketIcon/>
           <span className="nav__itemLineTwo nav__basketCount">0</span>
 
         </div>
+        
+         </Link>
       
     </div>
     </div>

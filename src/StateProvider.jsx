f84feb5ae508e5c48dfb85d79ec4f60b4825ec01
abhay kumar <abhaykumar2871999@gmail.com>
2024-07-03 18:42:52 +0530

    exported stateProvider in  StateProvider.jsx

--- src/StateProvider.jsx
+++ src/StateProvider.jsx
@@ -1,2 +1,11 @@
-import React , {createContext, useContext, useReducer} from "react";
-import {reducer} from "./reducer";
+import React, { createContext, useContext, useReducer } from "react";
+
+export const StateContext = createContext();
+
+export const StateProvider = ({ reducer, initialState, children }) => {
+  return (
+    <StateContext.Provider value={useReducer(reducer, initialState)}>
+      {children}
+    </StateContext.Provider>
+  );
+};

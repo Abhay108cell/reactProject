231747b6a14a0dbd5387abfa09974e52cacd3431
abhay kumar <abhaykumar2871999@gmail.com>
2024-07-04 22:45:42 +0530

    using props now in checkoutProduct.jsx

--- src/CheckoutProduct.jsx
+++ src/CheckoutProduct.jsx
@@ -1,19 +1,20 @@
 import React from 'react';
 import "./CheckoutProduct.css"
 
-function CheckoutProduct() {
+function CheckoutProduct(id, image, title, rating, price) {
   return (
     <div>
       <div className="checkoutProduct">
-        <img src="https://images-na.ssl-images-amazon.com/images/I/71mEsHyzSCL._SL1000_.jpg" alt="" className="checkoutProduct__image" />
+        <img src={image} alt="" className="checkoutProduct__image" />
         <div className="checkoutProduct__info">
-          <p className="checkoutProduct__title">The Lean Startup</p>
+          <p className="checkoutProduct__title">{title}</p>
           <p className="checkoutProduct__price">
           <small>$</small>
-          <strong>60</strong>
+          <strong>{price}</strong>
           </p>
           <div className="checkoutProduct__rating">
-            <p>⭐⭐⭐⭐⭐</p>
+          {Array(rating).fill().map((_, i) => ( <p>⭐</p>
+            ))}
           </div>
           <button>Remove from Cart</button>
         </div>
